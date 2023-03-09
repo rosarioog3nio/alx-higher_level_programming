55a38549246f5c9b4cc0000122df649154ac0cae
rosarioog3nio <rosarioogenio@gmail.com>
2023-03-09 15:26:24 -0800

    following the pycode style

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def add(a, b):
-	"""My addition function
+    """My addition function
 
-	Args:
-		a: first int
-		b: second int
+    Args:
+        a: first int
+        b: second int
 
-	Return:
-		The function value. a+ b
+        Return:
+        The function value. a+ b
 
-	"""
-	return (a + b)
+    """
+    return (a + b)
