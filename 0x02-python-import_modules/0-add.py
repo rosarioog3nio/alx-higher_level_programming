4650ec99c4952628077bbb1f121ef473663812cf
rosarioog3nio <rosarioogenio@gmail.com>
2023-03-09 14:31:37 -0800

    fixing indentation (for some reason gotta be space and not tab)

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
+#!/usr//bin/python3
 
-#!/usr/bin/python3
-def add(a, b):
-	a = 1
-	b = 2
-	print("a + b = {}".format(a + b))
-	return a + b
-add(1, 2)
+if __name__ == "__main__":
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first int
+        b: second int
+
+    Return:
+        The function value. a+ b
+
+    """
+    return (a + b)
